298de08ffcc83d71ecb80bead361afe4cce792b5
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2015-06-30 00:30:50 -0300

    second

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Document
+
+
 # Register your models here.
+@admin.register(Document)
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ['content', 'created_at', 'created_by']
+
--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import, unicode_literals, print_function
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-
+from model_utils import tracker
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
@@ -18,3 +19,8 @@ class Document(models.Model):
     modified_by = models.ForeignKey(to=USER_MODEL,
                                     related_name="%(app_label)s_%(class)s_modified_by", null=True,
                                     blank=True, on_delete=models.SET_NULL)
+
+    content_tracker = tracker.FieldTracker(fields=['content'])
+
+    def __unicode__(self):
+        return "{}".format(self.content)
